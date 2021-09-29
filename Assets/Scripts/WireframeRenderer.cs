09cbc6b975391af03d8b0fcf12d4685a132be43c
Petteri Timonen <>
2021-09-29 20:02:57 +0300

    Implement clipping

--- Assets/Scripts/WireframeRenderer.cs
+++ Assets/Scripts/WireframeRenderer.cs
@@ -1,20 +1,19 @@
-using UnityEngine;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using UnityEngine;
 
 public class WireframeRenderer : MonoBehaviour
 {
     public static WireframeRenderer Instance;
 
     [Header("Camera to render lines from. Use square aspect for improved preview.")]
-    [SerializeField] Camera renderCamera;
+    [SerializeField] private Camera renderCamera;
     [Header("Should we skip every 3rd line in triangles?")]
-    [SerializeField] bool drawQuads = false;
+    [SerializeField] private readonly bool drawQuads = false;
+    private ObservableCollection<MeshFilter> meshFilters = new ObservableCollection<MeshFilter>();
 
-    ObservableCollection<MeshFilter> meshFilters = new ObservableCollection<MeshFilter>();
-
-    class SkinnedObject
+    private class SkinnedObject
     {
         public SkinnedMeshRenderer skinnedMeshRenderer;
         public MeshFilter skinnedMeshFilter;
@@ -25,9 +24,10 @@ public class WireframeRenderer : MonoBehaviour
             this.skinnedMeshFilter = skinnedMeshFilter;
         }
     }
-    ObservableCollection<SkinnedObject> skinnedObjects = new ObservableCollection<SkinnedObject>();
 
-    class MeshCache
+    private ObservableCollection<SkinnedObject> skinnedObjects = new ObservableCollection<SkinnedObject>();
+
+    private class MeshCache
     {
         public Mesh mesh { get; private set; }
         public int[] triangles { get; private set; }
@@ -53,10 +53,10 @@ public class WireframeRenderer : MonoBehaviour
             vertices = mesh.vertices;
         }
     }
-    List<MeshCache> meshCache = new List<MeshCache>();
-    bool cacheRequiresUpdate = false;
 
-    AudioRender.IRenderDevice renderDevice;
+    private List<MeshCache> meshCache = new List<MeshCache>();
+    private bool cacheRequiresUpdate = false;
+    private AudioRender.IRenderDevice renderDevice;
 
     public void AddMesh(MeshFilter meshFilter)
     {
@@ -178,14 +178,14 @@ public class WireframeRenderer : MonoBehaviour
         {
             for (int i = 0; i < meshCache[cacheIndex].triangles.Length; i += 6)
             {
-                if (TriangleFacesCamera(cacheIndex, i))
+                // if (TriangleFacesCamera(cacheIndex, i))
                 {
                     DrawLine(cacheIndex, i + 0, i + 1);
                     DrawLine(cacheIndex, i + 1, i + 2);
                     DrawLine(cacheIndex, i + 2, i + 0);
                 }
 
-                if (TriangleFacesCamera(cacheIndex, i + 3))
+                // if (TriangleFacesCamera(cacheIndex, i + 3))
                 {
                     DrawLine(cacheIndex, i + 3, i + 4);
                     DrawLine(cacheIndex, i + 4, i + 5);
@@ -201,19 +201,38 @@ public class WireframeRenderer : MonoBehaviour
         {
             for (int i = 0; i < meshCache[cacheIndex].triangles.Length; i += 6)
             {
-                if (TriangleFacesCamera(cacheIndex, i))
+                // if (TriangleFacesCamera(cacheIndex, i))
                 {
                     DrawLine(cacheIndex, i + 0, i + 1);
                     DrawLine(cacheIndex, i + 1, i + 2);
                 }
 
-                if (TriangleFacesCamera(cacheIndex, i + 3))
+                // if (TriangleFacesCamera(cacheIndex, i + 3))
                 {
                     DrawLine(cacheIndex, i + 4, i + 5);
                     DrawLine(cacheIndex, i + 5, i + 3);
                 }
             }
         }
+    }
+
+    private Vector4 GetClipPoint(int cacheIndex, int triangleListIdx)
+    {
+        Vector3 localPoint = meshCache[cacheIndex].vertices[meshCache[cacheIndex].triangles[triangleListIdx]];
+        Vector3 worldPoint = meshCache[cacheIndex].transform.TransformPoint(localPoint);
+        Vector4 clipPoint = renderCamera.projectionMatrix * renderCamera.worldToCameraMatrix * new Vector4(worldPoint.x, worldPoint.y, worldPoint.z, 1.0f);
+        return clipPoint;
+    }
+
+    private Vector2 ClipToScopePoint(Vector4 clipPoint)
+    {
+        Vector3 ndcPoint = clipPoint / clipPoint.w;
+        Vector2 scopePoint = new Vector2(0.5f, -0.5f) * ndcPoint;
+
+        float aspectRatio = Screen.width / (float)Screen.height;
+        scopePoint.x *= aspectRatio;
+
+        return scopePoint;
     }
 
     private Vector3 GetScreenPoint(int cacheIndex, int triangleListIdx)
@@ -233,12 +252,12 @@ public class WireframeRenderer : MonoBehaviour
 
         return scopePoint;
     }
+
     private bool TriangleFacesCamera(int cacheIndex, int triangleListIdx)
     {
         Vector3 a = GetScreenPoint(cacheIndex, triangleListIdx);
         Vector3 b = GetScreenPoint(cacheIndex, triangleListIdx + 1);
         Vector3 c = GetScreenPoint(cacheIndex, triangleListIdx + 2);
-
         return Vector3.Cross(b - a, c - a).z < 0;
     }
 
@@ -248,12 +267,122 @@ public class WireframeRenderer : MonoBehaviour
         renderDevice.SetPoint(target);
     }
 
+    private bool ClipCylinder(ref Vector4 a, ref Vector4 b)
+    {
+        // Swap so that a.w <= b.w
+        if (a.w > b.w)
+        {
+            Vector4 temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Segment completely outside near/far plane.
+        if (a.z > a.w || b.z < -b.w)
+        {
+            return false;
+        }
+
+        // Find C = (1 - t) * a + t * b such that C_x^2 + C_y^2 = C_w^2
+        // First solve for t by formulating the problem as quadratic equation t^2*qa + t*qb + qc = 0
+        // Coefficients for solving the quadratic equation: FullSimplify[MonomialList[((t-1)*Subscript[A, x]+t*Subscript[B,x])^2+((t-1)*Subscript[A, y]+t*Subscript[B,y])^2-((t-1)*Subscript[A, w]+t*Subscript[B,w])^2, t]]
+        float qa = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) - (a.w - b.w) * (a.w - b.w);
+        float qb = 2.0f * ((b.x - a.x) * a.x + (b.y - a.y) * a.y + a.w * a.w - a.w * b.w);
+        float qc = a.x * a.x + a.y * a.y - a.w * a.w;
+        float det = qb * qb - 4 * qa * qc;
+
+        // Cull if no intersection between line and cylinder.
+        if (Mathf.Abs(qa) < Mathf.Epsilon || det < 0.0f)
+        {
+            return false;
+        }
+
+        // Solve intersection with cylinder.
+        float tA = (-qb - Mathf.Sqrt(det)) / (2.0f * qa);
+        float tB = (-qb + Mathf.Sqrt(det)) / (2.0f * qa);
+
+        // Swap so that tA <= tB
+        if (tA > tB)
+        {
+            float temp = tA;
+            tA = tB;
+            tB = temp;
+        }
+
+        // Solve intersection with near and far plane.
+        float tNear = (a.z + a.w) / (a.z + a.w - b.z - b.w);
+        float tFar = (a.z - a.w) / (b.w - a.w + a.z - b.z);
+
+        // Solve intersection points.
+        Vector4 pNear = (1 - tNear) * a + tNear * b;
+        Vector4 pFar = (1 - tFar) * a + tFar * b;
+        Vector4 pA = (1 - tA) * a + tA * b;
+        Vector4 pB = (1 - tB) * a + tB * b;
+
+        bool lineIntersectsWithNearCircle = pNear.x * pNear.x + pNear.y * pNear.y < pNear.w * pNear.w;
+        bool lineIntersectsWithFarCircle = pFar.x * pFar.x + pFar.y * pFar.y < pFar.w * pFar.w;
+
+        // Solve intersection combinations.
+        if (lineIntersectsWithFarCircle)
+        {
+            tA = lineIntersectsWithNearCircle ? tNear : (tA > tNear ? tA : tB);
+            tB = tFar;
+        }
+        else
+        {
+            if (lineIntersectsWithNearCircle)
+            {
+                if (pB.z < -pB.w)
+                {
+                    // Furthest cylinder intersection is behind near plane, cull.
+                    return false;
+                }
+                else
+                {
+                    // Clip to near plane.
+                    tA = tNear;
+                }
+            }
+            else if (pA.z < -pB.w)
+            {
+                // Ray does not intersect with near and far circle, AND does not lay between the circles because it hits the cylinder behind the near plane, cull.
+                return false;
+            }
+        }
+
+        // Clip to the original line segment.
+        tA = Mathf.Max(0.0f, tA);
+        tB = Mathf.Min(1.0f, tB);
+
+        // The line segment inside the frustrum does not overlap with the line segment being clipped.
+        if (tA > 1.0f || tB < 0.0f)
+        {
+            return false;
+        }
+
+        // Interpolate and output results.
+        Vector4 clippedA = (1 - tA) * a + tA * b;
+        Vector4 clippedB = (1 - tB) * a + tB * b;
+        a = clippedA;
+        b = clippedB;
+
+        return true;
+    }
+
     private void DrawLine(int cacheIndex, int triangleListIdxFrom, int triangleListIdxTo)
     {
-        Vector2 scopeFrom = GetScopePoint(cacheIndex, triangleListIdxFrom);
-        Vector2 scopeTo = GetScopePoint(cacheIndex, triangleListIdxTo);
+        Vector4 a = Vector4.zero;
+        Vector4 b = Vector4.zero;
 
-        renderDevice.SetPoint(scopeFrom);
-        renderDevice.DrawLine(scopeTo);
+        Vector4 clipFrom = GetClipPoint(cacheIndex, triangleListIdxFrom);
+        Vector4 clipTo = GetClipPoint(cacheIndex, triangleListIdxTo);
+
+        if (!ClipCylinder(ref clipFrom, ref clipTo))
+        {
+            return;
+        }
+
+        renderDevice.SetPoint(ClipToScopePoint(clipFrom));
+        renderDevice.DrawLine(ClipToScopePoint(clipTo));
     }
 }
